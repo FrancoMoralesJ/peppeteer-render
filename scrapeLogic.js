e09fe8e6d27ecc766a02f818d07b7779f1ff8d7d
Franco Morales <franco017@outlook.com>
2025-04-13 22:47:48 -0500

    integramos doker v1

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -3,7 +3,20 @@ const puppeteer=require("puppeteer");
 const send = require("send");
 const scrapeLogic=async (res)=>{
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch(
+        {
+            args: [
+                "--disable-setuid-sandbox",
+                "--no-sandbox",
+                "--single-process",
+                "--no-zygote",
+              ],
+              executablePath:
+                process.env.NODE_ENV === "production"
+                  ? process.env.PUPPETEER_EXECUTABLE_PATH
+                  : puppeteer.executablePath(),
+        }
+    );
 
     try {
              
